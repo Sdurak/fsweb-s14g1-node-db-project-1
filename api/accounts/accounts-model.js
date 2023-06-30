9d3afd142230dd003f85e8189ba7a38ed5356d77
Sdurak <sdurak91@hotmail.com>
2023-06-30 16:32:57 +0300

    S14G1 Proje

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,42 @@
-const getAll = () => {
-  // KODLAR BURAYA
-}
+const db = require("../../data/db-config");
 
-const getById = id => {
+const getAll = async (limit, sortBy, sortDir) => {
   // KODLAR BURAYA
-}
+  //select * from accounts
 
-const create = account => {
-  // KODLAR BURAYA
-}
+  limit = limit || (await db("accounts").length);
+  sortBy = sortBy || "id";
+  sortDir = sortDir || "asc";
+  return db("accounts").orderBy(sortBy, sortDir).limit(limit);
+};
 
-const updateById = (id, account) => {
+const getById = (id) => {
   // KODLAR BURAYA
-}
+  //default select için return array formatındadır. First yapınca object {} formatında dönüyor.
+  return db("accounts").where("id", id).first();
+};
 
-const deleteById = id => {
+const getByName = (name) => {
+  //select * from accounts where name = '{name}' limit 1
+  return db("accounts").where("name", name).first();
+};
+
+const create = async (account) => {
   // KODLAR BURAYA
-}
+  const [id] = await db("accounts").insert(account);
+  return getById(id);
+};
+
+const updateById = async (id, account) => {
+  // KODLAR BURAYA
+  await db("accounts").where("id", id).update(account);
+  return getById(id);
+};
+
+const deleteById = (id) => {
+  // KODLAR BURAYA
+  return db("accounts").where("id", id).del();
+};
 
 module.exports = {
   getAll,
@@ -24,4 +44,5 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+  getByName,
+};
